7769158e7c8fee001b5e797ed08cb16f306cade6
Abhilash Knox George <user.abhilash@gmail.com>
2022-12-28 16:11:39 +0530

    Added readme file

---
+++ AssignementTVOS/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  AssignementTVOS
+//
+//  Created by Abhilash k George on 27/12/22.
+//
+
+import Foundation
---
+++ AssignementTVOS/UniversityModel.swift
@@ -0,0 +1,8 @@
+//
+//  UniversityModel.swift
+//  AssignementTVOS
+//
+//  Created by Abhilash k George on 27/12/22.
+//
+
+import Foundation
